05262a613bbefad8d6c02e0cb2774cae21112bc0
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2022-12-15 14:29:25 +0700

    Delete newtest.js

--- static/JS/newtest.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('button').addEventListener('click', function() {
-	html2canvas(document.querySelector('.specific'), {
-		onrendered: function(canvas) {
-			// document.body.appendChild(canvas);
-		  return Canvas2Image.saveAsPNG(canvas);
-		}
-	});
-});
