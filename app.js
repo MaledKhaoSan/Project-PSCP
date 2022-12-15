34bb62d7529039f686e37bdf15c3998729f102c3
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2022-12-15 14:27:55 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// ยังไม่ได้ใช้งาน
-function myFunc() {
-    // document.getElementById("demo").innerHTML = 'The first paragraph (index 0) inside "main" is: ' + y[0].innerHTML;
-    document.getElementById("demo").innerHTML = username;
-    // return username + site
-    
-}
